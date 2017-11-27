435f7a55c9bd716e7ec4a030bdce8ce3717bba65
eranight <abelnigt@gmail.com>
2017-11-27 22:26:50 +0300

    working gameservice

--- matchmaker/src/main/java/ru/atom/matchmaker/service/GameService.java
+++ matchmaker/src/main/java/ru/atom/matchmaker/service/GameService.java
@@ -1,10 +1,12 @@
 package ru.atom.matchmaker.service;
 
-import okhttp3.OkHttpClient;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by Alexandr on 25.11.2017.
@@ -16,9 +18,30 @@ public class GameService {
 
     @Value("${url}")
     private String url;
+    @Value("${create}")
+    private String create;
+    @Value("${start}")
+    private String start;
 
     public long create(int playerCount) {
         logger.info("create new game for " + playerCount + "th players");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), "playerCount=" + playerCount))
+                .url(url + create)
+                .build();
+        logger.info(url + create);
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String responseString = response.body().string();
+                logger.info("success " + responseString);
+                return Long.valueOf(responseString);
+            } else {
+                logger.info("smth wrong");
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
         return -1;
     }
 
