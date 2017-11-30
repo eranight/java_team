a1db1bd8c79f45fef81ee2b834e1fb805d148a80
IlyaDolinskiy <dolinsky94@yandex.ru>
2017-12-01 00:59:12 +0300

    added a few more tests

--- matchmaker/src/test/java/ru/atom/matchmaker/component/MatchmakerComponentIntegrationTest.java
+++ matchmaker/src/test/java/ru/atom/matchmaker/component/MatchmakerComponentIntegrationTest.java
@@ -1,11 +1,8 @@
 package ru.atom.matchmaker.component;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,32 +11,31 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.junit.Assert.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(MatchmakerComponent.class)
 public class MatchmakerComponentIntegrationTest {
-//    @Autowired
-//    private MockMvc mockMvc;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MatchmakerComponent matchmakerComponent;
 
     @Test
     public void signupTest() throws Exception {
-        assertThat(this.restTemplate.postForLocation("/matchmaker/signup", "login=admin&password=1234").equals(status().isOk()));
-
-//        mockMvc.perform(post("/matchmaker/signup")
-//                .content("login=admin&password=1234")
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-//                .andExpect(status().isOk());
+        mockMvc.perform(post("/matchmaker/signup")
+                .content("login=admin&password=1234")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk())
+                .andExpect(result -> "ok".equals(result.getResponse().getContentAsString()));
     }
 
-//    @Test
-//    public void joinTest() throws Exception {
-//        mockMvc.perform(post("/matchmaker/join")
-//                .content("login=admin&password=1234")
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    public void joinTest() throws Exception {
+        mockMvc.perform(post("/matchmaker/join")
+                .content("login=admin&password=1234")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+    }
 }
