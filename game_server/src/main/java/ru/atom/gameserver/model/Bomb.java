fcb85abf81a6b5a0b497cd81619a377145f03040
IlyaDolinskiy <dolinsky94@yandex.ru>
2017-12-02 20:49:23 +0300

    added model

---
+++ game_server/src/main/java/ru/atom/gameserver/model/Bomb.java
@@ -0,0 +1,24 @@
+package ru.atom.gameserver.model;
+
+import ru.atom.gameserver.action.Tickable;
+
+public class Bomb extends GameObjectAbstract implements Tickable {
+    private int timer = 10;
+
+    public Bomb(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void tick(long elapsed) {
+        timer -= elapsed;
+        if (timer <= 0) {
+            blowUp();
+            isActive = false;
+        }
+    }
+
+    private void blowUp() {
+
+    }
+}
